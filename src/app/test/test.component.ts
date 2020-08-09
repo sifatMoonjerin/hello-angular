7b92567770ef38b15a47e3360e5482732d36ad30
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-08-09 19:19:50 +0600

    interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,10 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  template: `
+    <h3> Hello {{name}}</h3>
+    <h4>{{url}}</h4>
+  `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
+
+  public name = 'Sifat Moonjerin';
+  public url = window.location.href;
 
   constructor() { }
 
