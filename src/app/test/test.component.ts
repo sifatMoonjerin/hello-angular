1687d51c8bcd1f8e81fa714c6f82e17b033a7054
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-08-10 09:49:34 +0600

    property binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h3> Hello {{name}}</h3>
     <h4>{{url}}</h4>
+    <input type='text' value='hello' />
+    <input [disabled]='isDisabled' type='text' value='hello' />
   `,
   styleUrls: ['./test.component.css']
 })
@@ -12,6 +14,7 @@ export class TestComponent implements OnInit {
 
   public name = 'Sifat Moonjerin';
   public url = window.location.href;
+  public isDisabled = false;
 
   constructor() { }
 
